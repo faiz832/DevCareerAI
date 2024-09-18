079c78c9e64cd233e2b93dacffdc1cce7700725b
Faiz <faqodefazz@gmail.com>
2024-09-18 13:16:04 +0700

    style: change hover color register button

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -137,7 +137,7 @@
 
                             @if (Route::has('register'))
                                 <a href="{{ route('register') }}"
-                                    class="hidden md:inline rounded bg-blue-500 hover:bg-blue-700 border border-blue-500 hover:border-blue-700 py-2 px-4 font-semibold text-white text-center transition duration-300 ease-in-out">
+                                    class="hidden md:inline rounded bg-blue-500 hover:bg-blue-600 border border-blue-500 hover:border-blue-600 py-2 px-4 font-semibold text-white text-center transition duration-300 ease-in-out">
                                     Register
                                 </a>
                             @endif
