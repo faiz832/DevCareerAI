3ec79cd67994543cd3338322a8c87e72ccbe6f7b
Faiz <faqodefazz@gmail.com>
2024-09-21 07:51:36 +0700

    style: add sidebar menu active state

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -4,10 +4,13 @@
         <h2 class="text-lg font-bold text-slate-900 mb-4">MENU</h2>
         <ul class="space-y-4">
             <li>
-                <a href="{{ route('dashboard') }}" class="flex items-center text-blue-500 relative">
-                    <div class="absolute -left-6 h-full w-1 bg-blue-500"></div>
-                    <svg class="h-5 w-5 mr-3 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 24 24" stroke="currentColor">
+                <a href="{{ route('dashboard') }}"
+                    class="flex items-center {{ Route::is('dashboard') ? 'text-blue-500 relative' : 'text-slate-900' }}">
+                    @if (Route::is('dashboard'))
+                        <div class="absolute -left-6 h-full w-1 bg-blue-500"></div>
+                    @endif
+                    <svg class="h-5 w-5 mr-3 {{ Route::is('dashboard') ? 'text-blue-500' : 'text-slate-900' }}"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                     </svg>
@@ -16,9 +19,13 @@
             </li>
             @role('student')
                 <li>
-                    <a href="{{ route('mycourses.index') }}" class="flex items-center text-slate-900">
-                        <svg class="h-5 w-5 mr-3 text-slate-900" xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 24 24" stroke="currentColor">
+                    <a href="{{ route('mycourses.index') }}"
+                        class="flex items-center {{ Route::is('mycourses.index') ? 'text-blue-500 relative' : 'text-slate-900' }}">
+                        @if (Route::is('mycourses.index'))
+                            <div class="absolute -left-6 h-full w-1 bg-blue-500"></div>
+                        @endif
+                        <svg class="h-5 w-5 mr-3 {{ Route::is('mycourses.index') ? 'text-blue-500' : 'text-slate-900' }}"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
                         </svg>
@@ -28,9 +35,13 @@
             @endrole
             @role('owner')
                 <li>
-                    <a href="{{ route('admin.teachers.index') }}" class="flex items-center text-slate-900">
-                        <svg class="h-5 w-5 mr-3 text-slate-900" xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 24 24" stroke="currentColor">
+                    <a href="{{ route('admin.teachers.index') }}"
+                        class="flex items-center {{ Route::is('admin.teachers.index') ? 'text-blue-500 relative' : 'text-slate-900' }}">
+                        @if (Route::is('admin.teachers.index'))
+                            <div class="absolute -left-6 h-full w-1 bg-blue-500"></div>
+                        @endif
+                        <svg class="h-5 w-5 mr-3 {{ Route::is('admin.teachers.index') ? 'text-blue-500' : 'text-slate-900' }}"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path d="M12 14l9-5-9-5-9 5 9 5z" />
                             <path
                                 d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
@@ -41,9 +52,13 @@
                     </a>
                 </li>
                 <li>
-                    <a href="{{ route('admin.categories.index') }}" class="flex items-center text-slate-900">
-                        <svg class="h-5 w-5 mr-3 text-slate-900" xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 24 24" stroke="currentColor">
+                    <a href="{{ route('admin.categories.index') }}"
+                        class="flex items-center {{ Route::is('admin.categories.index') ? 'text-blue-500 relative' : 'text-slate-900' }}">
+                        @if (Route::is('admin.categories.index'))
+                            <div class="absolute -left-6 h-full w-1 bg-blue-500"></div>
+                        @endif
+                        <svg class="h-5 w-5 mr-3 {{ Route::is('admin.categories.index') ? 'text-blue-500' : 'text-slate-900' }}"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z" />
                         </svg>
@@ -53,9 +68,13 @@
             @endrole
             @role('owner|teacher')
                 <li>
-                    <a href="{{ route('admin.courses.index') }}" class="flex items-center text-slate-900">
-                        <svg class="h-5 w-5 mr-3 text-slate-900" xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 24 24" stroke="currentColor">
+                    <a href="{{ route('admin.courses.index') }}"
+                        class="flex items-center {{ Route::is('admin.courses.index') ? 'text-blue-500 relative' : 'text-slate-900' }}">
+                        @if (Route::is('admin.courses.index'))
+                            <div class="absolute -left-6 h-full w-1 bg-blue-500"></div>
+                        @endif
+                        <svg class="h-5 w-5 mr-3 {{ Route::is('admin.courses.index') ? 'text-blue-500' : 'text-slate-900' }}"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
                         </svg>
@@ -65,9 +84,13 @@
             @endrole
             @role('owner')
                 <li>
-                    <a href="{{ route('admin.subscribe_transactions.index') }}" class="flex items-center text-slate-900">
-                        <svg class="h-5 w-5 mr-3 text-slate-900" xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 24 24" stroke="currentColor">
+                    <a href="{{ route('admin.subscribe_transactions.index') }}"
+                        class="flex items-center {{ Route::is('admin.subscribe_transactions.index') ? 'text-blue-500 relative' : 'text-slate-900' }}">
+                        @if (Route::is('admin.subscribe_transactions.index'))
+                            <div class="absolute -left-6 h-full w-1 bg-blue-500"></div>
+                        @endif
+                        <svg class="h-5 w-5 mr-3 {{ Route::is('admin.subscribe_transactions.index') ? 'text-blue-500' : 'text-slate-900' }}"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z" />
                         </svg>
