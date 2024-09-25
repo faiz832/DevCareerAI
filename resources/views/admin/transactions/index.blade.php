ae9c6992446ac85988c2bdfc21454b394cdfa626
Faiz <faqodefazz@gmail.com>
2024-09-25 09:14:18 +0700

    add table in transactions page

--- resources/views/admin/transactions/index.blade.php
+++ resources/views/admin/transactions/index.blade.php
@@ -1,18 +1,58 @@
 <x-app-layout>
-    {{-- <div class="h-[500px] py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __('Dashboard') }}
-                </div>
-            </div>
-        </div>
-    </div> --}}
     <div class="flex">
         <div class="w-[1200px] relative flex items-start mx-auto p-4 py-6 lg:py-8 gap-8">
             @include('layouts.sidebar')
-            <!-- Main Content (Blank Space) -->
-            <div class="w-full lg:w-5/6 bg-slate-100 h-screen rounded-lg">
+            <!-- Main Content -->
+            <div class="w-full lg:w-5/6 h-screen">
+                <div class="space-y-6">
+                    <div class="text-2xl font-semibold">Transactions</div>
+
+                    <div class="rounded-lg px-6 pt-6 pb-2 shadow-lg border border-gray-200">
+                        <table id="dataTable" class="display" style="width:100%">
+                            <thead class="bg-gray-50">
+                                <tr>
+                                    <th
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        No</th>
+                                    <th
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Account</th>
+                                    <th
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Course</th>
+                                    <th
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Date</th>
+                                    <th
+                                        class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Status</th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        <div class="text-sm">1</div>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        <div class="text-sm">John Doe</div>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        <div class="text-sm">Fundamental of Web Developer</div>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        <div class="text-sm text-gray-900">2023-09-20</div>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-center">
+                                        <span
+                                            class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                            Done
+                                        </span>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
