ad0d6dcac1ac3821afdb563ad1f4e321b71b1623
Faiz <faqodefazz@gmail.com>
2024-09-25 08:55:36 +0700

    add table in categories page

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,18 +1,59 @@
 <x-app-layout>
-    {{-- <div class="h-[500px] py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __('Dashboard') }}
-                </div>
-            </div>
-        </div>
-    </div> --}}
     <div class="flex">
         <div class="w-[1200px] relative flex items-start mx-auto p-4 py-6 lg:py-8 gap-8">
             @include('layouts.sidebar')
-            <!-- Main Content (Blank Space) -->
-            <div class="w-full lg:w-5/6 bg-slate-100 h-screen rounded-lg">
+            <!-- Main Content -->
+            <div class="w-full lg:w-5/6 h-screen">
+                <div class="space-y-6">
+                    <div class="flex gap-4">
+                        <div class="text-2xl font-semibold">Add Category</div>
+                        <button
+                            class="rounded-lg bg-blue-500 hover:bg-blue-600 text-white font-semibold py-1 px-3 duration-300 ease-in-out">
+                            +
+                        </button>
+                    </div>
+                    <div class="rounded-lg px-6 pt-6 pb-2 shadow-lg border border-gray-200">
+                        <table id="dataTable" class="display" style="width:100%">
+                            <thead class="bg-gray-50">
+                                <tr>
+                                    <th
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        No</th>
+                                    <th
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Category</th>
+                                    <th
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Date</th>
+                                    <th
+                                        class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Action</th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        <div class="text-sm">1</div>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        <div class="text-sm">Web Developer</div>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        <div class="text-sm text-gray-900">2023-09-20</div>
+                                    </td>
+                                    <td class="px-6 py-4 text-center space-x-2">
+                                        <button
+                                            class="text-blue-500 hover:text-white font-semibold bg-white hover:bg-blue-500 border border-blue-500 rounded px-2 py-1 transition duration-300 ease-in-out">View</button>
+                                        <button
+                                            class="text-yellow-500 hover:text-white font-semibold bg-white hover:bg-yellow-500 border border-yellow-500 rounded px-2 py-1 transition duration-300 ease-in-out">Edit</button>
+                                        <button
+                                            class="text-red-500 hover:text-white font-semibold bg-white hover:bg-red-500 border border-red-500 rounded px-2 py-1 transition duration-300 ease-in-out">Delete</button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
