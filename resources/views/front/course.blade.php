87f2079d50ba0103580d4cd75108f0e8f26b44ce
Faiz <faqodefazz@gmail.com>
2024-09-28 16:32:31 +0700

    style: adjust course page style

--- resources/views/front/course.blade.php
+++ resources/views/front/course.blade.php
@@ -115,14 +115,10 @@
                             <!-- Title -->
                             <h1 class="font-semibold text-xl mb-2"><?= $content['title'] ?></h1>
 
-                            <!-- Short description -->
-                            {{-- <p class="text-md text-gray-600 mb-4"><?= $content['desc'] ?></p>
-
-                            <!-- Teacher info -->
-                            <p class="text-sm text-gray-600 mb-2">By Sandika Galih</p> --}}
-
                             <div class="flex flex-col justify-between h-full">
+                                <!-- Short description -->
                                 <p class="text-md text-gray-600 min-h-16"><?= $content['desc'] ?></p>
+                                <!-- Teacher info -->
                                 <p class="text-sm text-gray-600 mb-2">By Sandika Galih</p>
                             </div>
 
